2653b04955d2008d86869abfcac9683291387a9a
Logan Cockrell <loganmcockrell@gmail.com>
2024-03-05 20:28:38 -0600

    finished efficient branch again... oops
    forgot to get rid of the spaces between lines of code. looks a little better this way. still might not be as efficient though

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,11 @@
 nums = []
-
 for x in range(0,101):
     nums.append(x)
-
 for x in range(0, len(nums), 3):
     nums[x] = "Fizz"
-
 for x in range(0, len(nums), 5):
     nums[x] = "Buzz"
-
 for x in range(0, len(nums), 15):
     nums[x] = "FizzBuzz"
-
 for x in nums:
     print(x)
