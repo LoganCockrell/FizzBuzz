93c97c9f32548f64d697fa5170f1ba94fe59f5a8
Logan Cockrell <loganmcockrell@gmail.com>
2024-03-05 20:14:24 -0600

    readable branch complete
    kept the original fizzbuzz version as it was the 'cleanest'. added notes to each section explaining what everything did and spaced out the blocks so they are more separated

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,18 @@
+#loop to count up to 100 from 1
 for x in range(1,101):
+
+    #checks if the current number is divisible by 3 AND 5, if so prints "FizzBuzz", if not moves on to next statement
     if(x%3 == 0 and x%5 == 0):
         print("FizzBuzz")
+
+    #checks if the current number is divisible by 3, if so prints "Fizz", if not moves on    
     elif(x%3 == 0):
         print("Fizz")
+
+    #checks if the current number is divisible by 5, if so prints "Buzz", if not moves on    
     elif(x%5 == 0):
         print("Buzz")
+    
+    #none of the other statements were true, so it prints the number and then repeats the loop
     else:
         print(x)
